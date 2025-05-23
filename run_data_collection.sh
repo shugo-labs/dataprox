1dd1c3b2eb98308624f1ebaf7ae13b11df4a1abf
borgg-dev <data.deepmind@gmail.com>
2025-05-23 18:00:31 +0100

    remove unecessary folders

--- run_data_collection.sh
+++ run_data_collection.sh
@@ -8,10 +8,6 @@ apt install -y npm && npm install -g pm2
 # Install Python packages
 pip3 install scapy pandas numpy psutil websockets asyncio pymongo python-dotenv
 
-# Create directories for logs and data
-mkdir -p traffic_flood_detection/logs
-mkdir -p traffic_flood_detection/data
-
 # Start necessary services
 systemctl enable --now sysstat
 systemctl enable --now snmpd
